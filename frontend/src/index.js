61257c4084e5375e7c9bd98536dbd8884a64b1e5
João Pedro <joaopedrosilvarodrigues57@gmail.com>
2024-09-08 10:48:39 -0300

    nada a declarar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Routes from './routes'; 
 import { Provider } from 'react-redux'; 
-<<<<<<< HEAD
 import store from './store'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "slick-carousel/slick/slick.css";
@@ -10,10 +9,6 @@ import "slick-carousel/slick/slick-theme.css";
 
 
 
-=======
-import store from './store';
-import { AuthProvider } from './contexts/AuthContext';
->>>>>>> ffc408aec268f4e599cd8a648b1cb050b41a9df5
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
