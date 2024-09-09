3d64aa87712c95a49150bcdcc1456d655fb953fb
gusrberto <gustavorsdemelo@gmail.com>
2024-09-09 02:58:50 -0300

    as

--- frontend/src/store/modules/agendamento/sagas.js
+++ frontend/src/store/modules/agendamento/sagas.js
@@ -12,3 +12,28 @@ export function getAllAppointments() {
     return response;
 }
 
+export function postAppointment(id, data) {
+    const response = api.post(`/agendamento/${id}`, data)
+    .then(async () => {
+        await Swal.fire("Sucesso!", "Atendimento cadastrado com sucesso!", "success");
+    })
+    .catch(async (err) => {
+        if (err.response) {
+            await Swal.fire("Erro!", err.response.data.message, "error");
+        }
+    });
+
+    return response;
+}
+
+export function getAvailableSchedules(id, schedule) {
+    const response = api.get(`/agendamento/${id}`, { params: { schedule } })
+    .catch(async (err) => {
+        if (err.response) {
+            await Swal.fire("Erro!", err.response.data.message, "error");
+        }
+    });
+
+    return response;
+}
+
